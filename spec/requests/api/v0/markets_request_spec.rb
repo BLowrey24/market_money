8166af7522a4d75cae5727161550e95e9f586033
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-16 14:06:51 -0400

    refactor: split my one test for my market index to look better. test: added a test for market show endpoint and it passes

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Markets API' do
   describe 'api/v0/markets' do
-    it 'this endpoint works' do
+    it 'this endpoint is successful' do
       create_list(:market, 5)
 
       get '/api/v0/markets'
 
       expect(response).to be_successful
+    end
+
+    it 'the response gets parsed and turned into a ruby object and looks cleaner' do
+      create_list(:market, 5)
+
+      get '/api/v0/markets'
 
       markets = JSON.parse(response.body, symbolize_names: true)
 
@@ -39,6 +45,16 @@ RSpec.describe 'Markets API' do
         expect(market).to have_key(:lon)
         expect(market[:lon]).to be_a(String)
       end
+    end
+  end
+
+  describe '/api/v1/books/#{id}' do
+    it 'it is a successful endpoint' do
+      id = create(:market).id
+
+      get "/api/v0/markets/#{id}"
+
+      expect(response).to be_successful
     end
   end
 end
