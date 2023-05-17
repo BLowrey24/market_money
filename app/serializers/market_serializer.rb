12cf4f98c135b283100eb0eacb2317161bf1de36
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-17 13:01:10 -0400

    feat: added functionality for getting a vendor for a market

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,4 +1,8 @@
 class MarketSerializer
   include JSONAPI::Serializer
-  attributes :name, :street, :city, :county,  :state, :zip, :lat, :lon, :vendor_count
+  attributes :name, :street, :city, :county,  :state, :zip, :lat, :lon
+
+  attributes :vendor_count do |object|
+    object.vendors.count
+  end
 end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -77,4 +77,57 @@ RSpec.describe 'Markets API' do
       expect(error_detail).to eq("Couldn't find market with 'id'=12345678910.")
     end
   end
+
+  before(:each) do
+    @market_1 = create(:market)
+    @market_2 = create(:market)
+    vendor_1 = create(:vendor)
+    vendor_2 = create(:vendor)
+    vendor_3 = create(:vendor)
+    MarketVendor.create(market_id: @market_1.id, vendor_id: vendor_1.id)
+    MarketVendor.create(market_id: @market_1.id, vendor_id: vendor_2.id)
+    MarketVendor.create(market_id: @market_2.id, vendor_id: vendor_3.id)
+    MarketVendor.create(market_id: @market_2.id, vendor_id: vendor_1.id)
+  end
+
+  describe '/api/v0/markets/#{market.id}/vendors' do
+    it 'returns all of the for a market vendors' do
+      get "/api/v0/markets/#{@market_1.id}/vendors"
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(body.count).to eq(2)
+
+      body.each do |vendor|
+        expect(vendor).to have_key(:id)
+        expect(vendor[:id]).to be_a(String)
+
+        expect(vendor).to have_key(:type)
+        expect(vendor[:type]).to eq('vendor')
+
+        expect(vendor).to have_key(:attributes)
+        expect(vendor[:attributes]).to be_a(Hash)
+
+        attributes = vendor[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes).to have_key(:description)
+        expect(attributes).to have_key(:contact_name)
+        expect(attributes).to have_key(:contact_phone)
+        expect(attributes).to have_key(:credit_accepted)
+      end
+    end
+
+    it 'returns an error if the id does not match any existing markets id' do
+      get "/api/v0/markets/1/vendors"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      
+      body = JSON.parse(response.body, symbolize_names: true)
+      error = body[:errors].first
+      
+      expect(error[:detail]).to eq("Could not find Market with id of 1.")
+    end
+  end
 end
