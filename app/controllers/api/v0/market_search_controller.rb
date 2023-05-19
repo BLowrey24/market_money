419e97e026bebd118343dc152fd8dd92f98e494d
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-19 00:51:07 -0400

    feat: add functionality for market search sad path for city only

--- app/controllers/api/v0/market_search_controller.rb
+++ app/controllers/api/v0/market_search_controller.rb
@@ -1,6 +1,22 @@
 class Api::V0::MarketSearchController < ApplicationController
+  before_action :invalid_params, only: [:index]
+
   def index
     @markets = MarketSearchFacade.search_markets(params[:name], params[:state], params[:city])
     render json: MarketSerializer.new(@markets)
+  end
+
+  private 
+
+  def invalid_params
+    return invalid if city_only
+  end
+
+  def city_only
+    params[:city].present? && params[:name].blank? && params[:state].blank?
+  end
+
+  def invalid
+    render json: { errors: [{ detail: "Invalid parameters."}] }, status: :unprocessable_entity
   end
 end
