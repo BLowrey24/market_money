4fc72f29aa53ccd051a250562904e14952a90035
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-19 00:56:32 -0400

    feat: add functionality for city_and_name market search sad path

--- app/controllers/api/v0/market_search_controller.rb
+++ app/controllers/api/v0/market_search_controller.rb
@@ -9,11 +9,15 @@ class Api::V0::MarketSearchController < ApplicationController
   private 
 
   def invalid_params
-    return invalid if city_only
+    return invalid if city_only || city_and_name
   end
 
   def city_only
     params[:city].present? && params[:name].blank? && params[:state].blank?
+  end
+
+  def city_and_name
+    params[:city].present? && params[:name].present? && params[:state].blank?
   end
 
   def invalid
